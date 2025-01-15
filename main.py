f26e6cbf36abc92ec5edda301e1af80169b3dd75
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-15 23:13:16 +0000

    created a website where you can get recipes and shopping list with ingredients needed, website styled with bootstrap5 and css

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import subprocess
 from build_menu import build_main_menu
+from flask_bootstrap import Bootstrap5
+import json
+
 
 app = Flask(__name__)
+Bootstrap5(app)
+
 
 # sample data
 
@@ -10,6 +15,14 @@ sample_data = {
     "message": "Hello Flask API world!",
     "status": "success"
 }
+
+global_menu = []
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/sample', methods=['GET'])
 def get_data():
@@ -19,6 +32,20 @@ def get_data():
 def get_menu():
     built_menu = build_main_menu()
     return jsonify(built_menu)
+
+@app.route('/recipes', methods=['GET'])
+def get_recipes():
+    global global_menu
+    global_menu = build_main_menu()
+    return render_template('recipes.html', menu=global_menu)
+
+@app.route('/shopping_list')
+def make_list():
+    shopping_list = []
+    for recipe in global_menu:
+        shopping_list.extend(recipe['ingredients'])
+    return render_template('shopping_list.html', shopping_list=shopping_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
