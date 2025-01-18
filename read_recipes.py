56bfc6d8a3a8ed6c06f04398f0d775b4e3514217
alegradi <adam.legradi@protonmail.ch>
2025-01-18 21:47:51 +0000

    ensure utf-8 encoding for with file open

--- read_recipes.py
+++ read_recipes.py
@@ -1,10 +1,10 @@
 import json
 
-with open('recipes/main_recipes.json', 'r') as file:
+with open('recipes/main_recipes.json', 'r', encoding='utf-8') as file:
     file_main_recipes = json.load(file)
     main_recipes = file_main_recipes["recipes"]
 
-with open('recipes/dessert_recipes.json', 'r') as file:
+with open('recipes/dessert_recipes.json', 'r', encoding='utf-8') as file:
     file_desserts = json.load(file)
     desserts = file_desserts["recipes"]
 
