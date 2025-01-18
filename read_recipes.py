bb8a6302811876463839d351b9a09bbb20244f76
alegradi <adam.legradi@protonmail.ch>
2025-01-18 21:48:11 +0000

    add module docstring

--- read_recipes.py
+++ read_recipes.py
@@ -1,5 +1,11 @@
 import json
 
+"""
+read_recipes.py
+
+Purpose is to read in recipes to be used by other functions
+"""
+
 with open('recipes/main_recipes.json', 'r', encoding='utf-8') as file:
     file_main_recipes = json.load(file)
     main_recipes = file_main_recipes["recipes"]
