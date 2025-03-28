23d035f89e1ce9c94582696222e99b8dc4e3be49
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-03-28 14:45:16 +0000

    fix the errors with pylint

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
+"""
+This configuration file contains global settings for the application,
+including categories of ingredients and common stop words that should
+be ignored when processing ingredient names.
+"""
+
 # config.py
 from categories import vegetables, fruits, meats, spices, aisle_products, fridge_freezer
 
@@ -11,6 +17,12 @@ CATEGORIES = {
     "Fridge & Freezer": [item.lower() for item in fridge_freezer]
 }
 
+"""
+CATEGORIES dictionary maps ingredient categories to lists of lowercase
+ingredient names. This helps categorize ingredients for shopping lists and
+recipe management.
+"""
+
 # Define common stop words
 STOP_WORDS = {"see note", "coarsely", "grated","and", "very", "finely",
               "chopped", "sliced", "diced",
@@ -22,3 +34,9 @@ STOP_WORDS = {"see note", "coarsely", "grated","and", "very", "finely",
               "if unavailable, use the grated zest of 1 lime",
               "if unavailable, use the zest of 1 lime","to serve",
               "boneless", "skinless"}
+
+"""
+STOP_WORDS is a set of common words and phrases that should be ignored
+during ingredient processing. These words are usually descriptive and do
+not affect the core ingredient name (e.g., "finely chopped", "fresh", etc.).
+"""
